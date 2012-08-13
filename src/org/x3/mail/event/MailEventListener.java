764720ffdcb4a9e18af3db75387e07d6b3e080d9
x3chaos <x3chaos@gmail.com>
2012-08-13 12:41:46 -0500

    Added listener
    Created an empty mail listener, to be used later

---
+++ src/org/x3/mail/event/MailEventListener.java
@@ -0,0 +1,13 @@
+package org.x3.mail.event;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+
+public class MailEventListener implements Listener {
+	
+	@EventHandler
+	public void onMessageSent(MessageSentEvent event) {
+		
+	}
+
+}
